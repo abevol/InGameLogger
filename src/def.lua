7ad604733285127d1a41c6d9f74125eebc65cae3
abevol <abevol@pm.me>
2024-07-06 05:59:36 +0800

    remove e.g. codes

--- src/def.lua
+++ src/def.lua
@@ -3,11 +3,6 @@ local public = {}
 
 -- document whatever you made publicly available to other plugins here
 -- use luaCATS annotations and give descriptions where appropriate
---  e.g. 
---	---@param a integer helpful description
---	---@param b string helpful description
---	---@return table c helpful description
---	function public.do_stuff(a, b) end
 
 ---@class InGameLoggerArgs
 ---@field public Name string
